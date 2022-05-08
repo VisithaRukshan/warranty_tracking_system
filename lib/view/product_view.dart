3198c289e7f0e2021cd39e974fd92b526400f5d2
visitha Perera <vishsptfy@gmail.com>
2022-05-08 18:06:41 +0530

    completed the project

--- lib/view/product_view.dart
+++ lib/view/product_view.dart
@@ -14,6 +14,7 @@ class ProductView extends StatefulWidget {
 class _ProductViewState extends State<ProductView> {
   List productList = [];
 
+
   @override
   void initState() {
     super.initState();
